d0e19636ac84c6b02a139e961cea0eca78db8eb2
hendercine <james@hendercine.com>
2018-02-26 16:36:05 -0800

    Extract and modify AsyncTask Superclass, modify MyEndpoint and add listener to MainActivity.

--- backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
+++ backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
@@ -3,7 +3,6 @@ package com.udacity.gradle.builditbigger.backend;
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
-import com.google.api.server.spi.config.Named;
 import com.hendercine.android.javajoker.JokeSource;
 
 /** An endpoint class we are exposing */
@@ -19,19 +18,12 @@ import com.hendercine.android.javajoker.JokeSource;
 public class MyEndpoint {
 
     /** A simple endpoint method that pulls a joke from the JokeSource and returns it */
-    @ApiMethod(name = "sayHi")
-    public MyBean sayHi(@Named("name") String name) {
+    @ApiMethod(name = "getJokeFromServer")
+    public MyBean getJokeFromServer() {
         MyBean response = new MyBean();
         response.setData(JokeSource.getJoke());
 
         return response;
     }
-    //    @ApiMethod(name = "getJokeFromServer")
-//    public MyBean getJokeFromServer() {
-//        MyBean response = new MyBean();
-//        response.setData(JokeSource.getJoke());
-//
-//        return response;
-//    }
 
 }
